d1b8fb9e61edc3c9ae5a64a4607abcbc5d4b01ae
xuanhieu75 <xuanhieu751999@gmail.com>
2022-01-13 15:02:27 +0700

    add doctor update form

--- src/update-info/UserUpdateInfo.js
+++ src/update-info/UserUpdateInfo.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './UserUpdateInfo.css'
+import './DoctorUpdateInfo.css'
 
 function UserUpdateInfo() {
 
     const author = 'Xuân Hiếu';
     const role = 'Chủ tài khoản';
+    const role2 = 'Bác sĩ';
   return (
     <>
       <h1>SỬA THÔNG TIN CÁ NHÂN</h1>
@@ -58,6 +60,56 @@ function UserUpdateInfo() {
       {/* <div>
         <img src="https://isofhcare-backup.s3-ap-southeast-1.amazonaws.com/images/frame_1c75cb90_c90f_433b_a75f_8bb7a87dafe0.png"/>
       </div> */}
+
+
+<h1>SỬA THÔNG TIN BÁC SĨ</h1>
+      <div className="form">
+        <form>
+          <div className="form-header">
+            <div className="form-header_avatar"></div>
+            <h3 className="form-header_name">{author}</h3>
+            <p className="form-header_role">{role2}</p>
+          </div>
+          <div className="form-content">
+            <div className="form-content_nameDoctor">
+              <label>Họ và tên</label>
+              <input type={Text} />
+            </div>
+            <div className="form-content_emailDoctor">
+              <label>Email</label>
+              <input type={"text"} />
+            </div>
+            <div className="form-content_hospital">
+              <label>Bệnh viên</label>
+              <input type={"text"} />
+            </div>
+            <div className="form-content_major">
+              <label>Chuyên khoa</label>
+              <select>
+                <option>Chọn chuyên khoa</option>
+                <option>...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="form-content_degree">
+              <label>Học vị</label>
+              <input type={"text"} />
+            </div>
+            <div className="form-content_exp">
+              <label>Năm kinh nghiệm</label>
+              <input type={"text"} />
+            </div>
+            <div className="form-content_time">
+              <label>Thời lượng tư vấn</label>
+              <input type={"text"} />
+            </div>
+          <div className="btn">
+          <button type="submit">Lưu</button>
+          <button type="reset">Hủy</button>
+          </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
