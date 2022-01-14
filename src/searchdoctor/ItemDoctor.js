96189580a6a51fdd998ae6ad9c8d49d747b61c05
geldun98 <geldun98@gmail.com>
2022-01-14 23:59:16 +0700

    commit

--- src/searchdoctor/ItemDoctor.js
+++ src/searchdoctor/ItemDoctor.js
@@ -14,16 +14,12 @@ const ItemDoctor = ({ data }) => {
       </h2>
       <p className="ItemDoctor-Work">{data.work}</p>
       <p className="ItemDoctor-Exp"> {data.experience} năm kinh nghiệm</p>
-{/* <<<<<<< HEAD
-      <div className="ItemDoctor-Major"> Chuyên khoa: {data.major}</div>   
-      <button>Đặt khám</button>
-======= */}
+
       <div className="ItemDoctor-Major"> Chuyên khoa: {data.major}</div>
 
       <Link to={url}>
         <button>Đặt khám</button>
       </Link>
-{/* >>>>>>> main */}
     </div>
   );
 };
