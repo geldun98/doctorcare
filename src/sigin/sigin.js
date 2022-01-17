76521c936d35b435066f3166965d51bacb05fe4e
Minh1997123 <mrminhdodo97@gmail.com>
2022-01-17 14:52:26 +0700

    css cho đăng ký đnăg nhập

--- src/sigin/sigin.js
+++ src/sigin/sigin.js
@@ -1,11 +1,12 @@
 /* eslint-disable react/jsx-pascal-case */
 import React from 'react';
 import Input_item from './components/Input_item'
+import './components/index.css'
 function Sigin() {
   return <div>
             <form>
-            <Input_item type="text" labelText="Tên Đăng nhập"/>
-            <Input_item type="password" labelText="Mật khẩu"/>
+            <Input_item type="text" labelText="Tên Đăng nhập"  />
+            <Input_item type="password" labelText="Mật khẩu" />
             <button type='submit'>đăng nhập</button>
             </form>
         </div>;
