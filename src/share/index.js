3f05416d220b1410983242caf24d95e8167b0c13
geldun98 <geldun98@gmail.com>
2022-01-19 21:52:50 +0700

    day7

--- src/share/index.js
+++ src/share/index.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import ListPost from './ListPost';
 import DetailPost from './DetailPost';
 import CreatePost from '../createpost';
 import './style.scss';
+import Context from '../store/Context';
 const Share = () => {
   const [showButton, setShowButton] = useState(true);
+  const [dataUser, setDataUser] = useContext(Context);
+  let role = dataUser.role;
+  if (role === 'doctor') {
+    role = true;
+  }
   return (
     <div className="Share">
       <Routes>
@@ -13,16 +19,18 @@ const Share = () => {
           path="/"
           element={
             <div>
-              <Link to="createpost">
-                <button
-                  className="Share-create"
-                  onClick={() => {
-                    setShowButton(!showButton);
-                  }}
-                >
-                  Tạo bài viết
-                </button>
-              </Link>
+              {role && (
+                <Link to="createpost">
+                  <button
+                    className="Share-create"
+                    onClick={() => {
+                      setShowButton(!showButton);
+                    }}
+                  >
+                    Tạo bài viết
+                  </button>
+                </Link>
+              )}
               <ListPost></ListPost>
             </div>
           }
