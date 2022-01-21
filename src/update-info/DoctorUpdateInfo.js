f8a9a7ffab2aa3522fdf3c413e804008fd5e307a
xuanhieu75 <xuanhieu751999@gmail.com>
2022-01-21 16:28:00 +0700

    validate update form

--- src/update-info/DoctorUpdateInfo.js
+++ src/update-info/DoctorUpdateInfo.js
@@ -12,18 +12,18 @@ function DoctorUpdateInfo() {
   useEffect(() => {
     const fetchUpdate = async () => {
       await updateApi.get(id).then((res) => {
-        nameValueInput.current.value = res.data.name;
-        emailValueInput.current.value = res.data.email;
-        hospitalValueInput.current.value = res.data.hospital;
-        majorValueInput.current.value = res.data.major;
-        degreeValueInput.current.value = res.data.degree;
-        expValueInput.current.value = res.data.exp;
-        timeValueInput.current.value = res.data.time;
+        if (res.data.name) nameValueInput.current.value = res.data.name;
+        if (res.data.email) emailValueInput.current.value = res.data.email;
+        if (res.data.hospital)
+          hospitalValueInput.current.value = res.data.hospital;
+        if (res.data.major) majorValueInput.current.value = res.data.major;
+        if (res.data.degree) degreeValueInput.current.value = res.data.degree;
+        if (res.data.exp) expValueInput.current.value = res.data.exp;
+        if (res.data.time) timeValueInput.current.value = res.data.time;
       });
     };
     fetchUpdate();
   }, []);
-
   function handleUpdate() {
     const data = {
       id: dataUser.id,
@@ -35,13 +35,61 @@ function DoctorUpdateInfo() {
       exp: expValueInput.current.value,
       time: timeValueInput.current.value,
     };
-    updateApi.update(data).then((res) => {
-      console.log(res.status);
-      if (res.status) {
-        setResult(true);
-      } else setReject(true);
-    });
+    if (
+      (nameValueInput.current.value !== "") &
+      (emailValueInput.current.value !== "") &
+      (hospitalValueInput.current.value !== "") &
+      (majorValueInput.current.value !== "") &
+      (degreeValueInput.current.value !== "") &
+      (expValueInput.current.value !== "") &
+      (timeValueInput.current.value !== "")
+    ) {
+      updateApi.update(data).then((res) => {
+        console.log(res.status);
+        if (res.status) {
+          setResult(true);
+        } else setReject(true);
+      });
+    } else {
+      setReject(true);
+    }
+    if (nameValueInput.current.value === "") {
+      errorName.current.classList.remove('errormsg_none');
+      errorName.current.classList.add('errormsg_block');
+    }
+    if (emailValueInput.current.value === "" || !validateEmail(emailValueInput.current.value)) {
+      errorEmail.current.classList.remove('errormsg_none');
+      errorEmail.current.classList.add('errormsg_block');
+    }
+    if (hospitalValueInput.current.value === "") {
+      errorHospital.current.classList.remove('errormsg_none');
+      errorHospital.current.classList.add('errormsg_block');
+    }
+    if (majorValueInput.current.value === "") {
+      errorMajor.current.classList.remove('errormsg_none');
+      errorMajor.current.classList.add('errormsg_block');
+    }
+    if (degreeValueInput.current.value === "") {
+      errorDegree.current.classList.remove('errormsg_none');
+      errorDegree.current.classList.add('errormsg_block');
+    }
+    if (expValueInput.current.value === "" || isNaN(expValueInput.current.value)) {
+      errorExp.current.classList.remove('errormsg_none');
+      errorExp.current.classList.add('errormsg_block');
+    }
+    if (timeValueInput.current.value === "" || isNaN(timeValueInput.current.value)) {
+      errorTime.current.classList.remove('errormsg_none');
+      errorTime.current.classList.add('errormsg_block');
+    } 
   }
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const nameValueInput = useRef();
   const emailValueInput = useRef();
@@ -50,6 +98,13 @@ function DoctorUpdateInfo() {
   const degreeValueInput = useRef();
   const expValueInput = useRef();
   const timeValueInput = useRef();
+  const errorName = useRef();
+  const errorEmail = useRef();
+  const errorHospital = useRef();
+  const errorMajor = useRef();
+  const errorDegree = useRef();
+  const errorExp = useRef();
+  const errorTime = useRef();
   return (
     <>
       <div className="form">
@@ -58,40 +113,36 @@ function DoctorUpdateInfo() {
         <form>
           <div className="form-header">
             <div className="form-header_avatar"></div>
-            <h3 className="form-header_name">Tài khoản : {dataUser.username}</h3>
+            <h3 className="form-header_name">
+              Tài khoản : {dataUser.username}
+            </h3>
             <p className="form-header_role">{dataUser.role}</p>
           </div>
           <div className="form-content">
             <div className="form-content_nameDoctor">
               <label>Họ và tên</label>
-              <input
-                type="text"
-                ref={nameValueInput}
-                // value={dataDoctor.name}
-              />
+              <input type="text" ref={nameValueInput} />
+              <p className="errormsg_none errname " ref={errorName}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="form-content_emailDoctor">
               <label>Email</label>
-              <input
-                type="text"
-                ref={emailValueInput}
-                // value={dataDoctor.email}
-              />
+              <input type="text" ref={emailValueInput} />
+              <p className="errormsg_none errname " ref={errorEmail}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="form-content_hospital">
               <label>Bệnh viên</label>
-              <input
-                type="text"
-                ref={hospitalValueInput}
-                // value={dataDoctor.hospital}
-              />
+              <input type="text" ref={hospitalValueInput} />
+              <p className="errormsg_none errname " ref={errorHospital}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="form-content_major">
               <label>Chuyên khoa</label>
-              <select
-                ref={majorValueInput}
-                //  value={dataDoctor.major}
-              >
+              <select ref={majorValueInput}>
                 <option>Chọn chuyên khoa</option>
                 <option>Tai mũi họng</option>
                 <option>Nội tiết</option>
@@ -101,30 +152,30 @@ function DoctorUpdateInfo() {
                 <option>Nội tổng hợp</option>
                 <option>Da liễu</option>
               </select>
+              <p className="errormsg_none errname " ref={errorMajor}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="form-content_degree">
               <label>Học vị</label>
-              <input
-                type="text"
-                ref={degreeValueInput}
-                // value={dataDoctor.degree}
-              />
+              <input type="text" ref={degreeValueInput} />
+              <p className="errormsg_none errname " ref={errorDegree}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="form-content_exp">
               <label>Năm kinh nghiệm</label>
-              <input
-                type="text"
-                ref={expValueInput}
-                //  value={dataDoctor.exp}
-              />
+              <input type="text" ref={expValueInput} />
+              <p className="errormsg_none errname " ref={errorExp}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="form-content_time">
               <label>Thời lượng tư vấn</label>
-              <input
-                type="text"
-                ref={timeValueInput}
-                // value={dataDoctor.time}
-              />
+              <input type="text" ref={timeValueInput} />
+              <p className="errormsg_none errname " ref={errorTime}>
+                Vui lòng nhập họ tên!
+              </p>
             </div>
             <div className="btn">
               <button type="button" onClick={handleUpdate}>
@@ -133,12 +184,18 @@ function DoctorUpdateInfo() {
               <button type="reset">Hủy</button>
             </div>
             {result && (
-              <p className="update-success" style={{ color: "green" }}>
+              <p
+                className="update-success"
+                style={{ color: "green", marginLeft: "20px" }}
+              >
                 Update thành công!
               </p>
             )}
             {reject && (
-              <p className="update-fail" style={{ color: "red" }}>
+              <p
+                className="update-fail"
+                style={{ color: "red", marginLeft: "20px" }}
+              >
                 Update thất bại!
               </p>
             )}
--- src/update-info/UserUpdateInfo.js
+++ src/update-info/UserUpdateInfo.js
@@ -3,6 +3,7 @@ import "./UserUpdateInfo.scss";
 import "./DoctorUpdateInfo.scss";
 import updateApi from "../api/updateApi";
 import Context from "../store/Context";
+import { getImageListItemBarUtilityClass } from "@mui/material";
 function UserUpdateInfo() {
   const [result, setResult] = useState(false);
   const [reject, setReject] = useState(false);
@@ -13,13 +14,27 @@ function UserUpdateInfo() {
   useEffect(() => {
     const fetchUpdate = async () => {
       await updateApi.get(id).then((res) => {
-        nameValueInput.current.value = res.data.name;
-        emailValueInput.current.value = res.data.email;
-        dobValueInput.current.value = res.data.dob;
-        genderValueInput.current.value = res.data.gender;
-        phoneValueInput.current.value = res.data.phone;
-        addressValueInput.current.value = res.data.address;
-        jobValueInput.current.value = res.data.job;
+        if (res.data.name) {
+          nameValueInput.current.value = res.data.name;
+        }
+        if (res.data.email) {
+          emailValueInput.current.value = res.data.email;
+        }
+        if (res.data.dob) {
+          dobValueInput.current.value = res.data.dob;
+        }
+        if (res.data.gender) {
+          genderValueInput.current.value = res.data.gender;
+        }
+        if (res.data.phone) {
+          phoneValueInput.current.value = res.data.phone;
+        }
+        if (res.data.address) {
+          addressValueInput.current.value = res.data.address;
+        }
+        if (res.data.Job) {
+          jobValueInput.current.value = res.data.Job;
+        }
       });
     };
     fetchUpdate();
@@ -27,7 +42,7 @@ function UserUpdateInfo() {
 
   function handleUpdate() {
     const data = {
-      id: dataUsers.id,
+      id: dataUser.id,
       name: nameValueInput.current.value,
       email: emailValueInput.current.value,
       dob: dobValueInput.current.value,
@@ -36,12 +51,72 @@ function UserUpdateInfo() {
       address: addressValueInput.current.value,
       Job: jobValueInput.current.value,
     };
-    updateApi.update(data).then((res) => {
-      if (res.status) {
-        setResult(true);
-      } else setReject(true);
-    });
+    if (
+      (nameValueInput.current.value !== "") &
+      (emailValueInput.current.value !== "") &
+      (dobValueInput.current.value !== "") &
+      (genderValueInput.current.value !== "") &
+      (phoneValueInput.current.value !== "") &
+      (addressValueInput.current.value !== "") &
+      (jobValueInput.current.value !== "")
+    ) {
+      updateApi.update(data).then((res) => {
+        if (res.status) {
+          setResult(true);
+        } else setReject(true);
+      });
+    } else {
+      setReject(true);
+    }
+    if (nameValueInput.current.value === "") {
+      errorName.current.classList.remove("errormsg_none");
+      errorName.current.classList.add("errormsg_block");
+    }
+    if (
+      emailValueInput.current.value === "" ||
+      !validateEmail(emailValueInput.current.value)
+    ) {
+      errorEmail.current.classList.remove("errormsg_none");
+      errorEmail.current.classList.add("errormsg_block");
+    }
+    if (dobValueInput.current.value === "") {
+      errorDob.current.classList.remove("errormsg_none");
+      errorDob.current.classList.add("errormsg_block");
+    }
+    if (genderValueInput.current.value === "") {
+      errorGender.current.classList.remove("errormsg_none");
+      errorGender.current.classList.add("errormsg_block");
+    }
+    if (
+      phoneValueInput.current.value === "" ||
+      !validatephone(phoneValueInput.current.value)
+    ) {
+      errorPhone.current.classList.remove("errormsg_none");
+      errorPhone.current.classList.add("errormsg_block");
+    }
+    if (addressValueInput.current.value === "") {
+      errorAddress.current.classList.remove("errormsg_none");
+      errorAddress.current.classList.add("errormsg_block");
+    }
+    if (jobValueInput.current.value === "") {
+      errorJob.current.classList.remove("errormsg_none");
+      errorJob.current.classList.add("errormsg_block");
+    }
   }
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const validatephone = (str) => {
+    return /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(
+      str
+    );
+  };
 
   const nameValueInput = useRef();
   const emailValueInput = useRef();
@@ -50,6 +125,13 @@ function UserUpdateInfo() {
   const phoneValueInput = useRef();
   const addressValueInput = useRef();
   const jobValueInput = useRef();
+  const errorName = useRef();
+  const errorEmail = useRef();
+  const errorDob = useRef();
+  const errorGender = useRef();
+  const errorPhone = useRef();
+  const errorAddress = useRef();
+  const errorJob = useRef();
   return (
     <div className="form">
       <h1>SỬA THÔNG TIN CÁ NHÂN</h1>
@@ -64,14 +146,23 @@ function UserUpdateInfo() {
           <div className="form-content_name">
             <label>Họ và tên</label>
             <input type="text" ref={nameValueInput} />
+            <p className="errormsg_none errname " ref={errorName}>
+              Vui lòng nhập họ tên!
+            </p>
           </div>
           <div className="form-content_email">
             <label>Email</label>
             <input type="text" ref={emailValueInput} />
+            <p className="errormsg_none" ref={errorEmail}>
+              Vui lòng nhập email!
+            </p>
           </div>
           <div className="form-content_dob">
             <label>Ngày sinh</label>
             <input type="date" ref={dobValueInput} />
+            <p className="errormsg_none" ref={errorDob}>
+              Vui lòng nhập ngày sinh!
+            </p>
           </div>
           <div className="form-content_gender">
             <label>Giới tính</label>
@@ -79,19 +170,32 @@ function UserUpdateInfo() {
               <option>Chọn giới tính</option>
               <option>Nam</option>
               <option>Nữ</option>
+              <option>Khác</option>
             </select>
+            <p className="errormsg_none" ref={errorGender}>
+              Vui lòng chọn giới tính!
+            </p>
           </div>
           <div className="form-content_phone">
             <label>Số điện thoại</label>
             <input type="text" ref={phoneValueInput} />
+            <p className="errormsg_none" ref={errorPhone}>
+              Vui lòng nhập số điện thọa!
+            </p>
           </div>
           <div className="form-content_address">
             <label>Địa chỉ</label>
             <input type="text" ref={addressValueInput} />
+            <p className="errormsg_none" ref={errorAddress}>
+              Vui lòng nhập địa chỉ!
+            </p>
           </div>
           <div className="form-content_job">
             <label>Nghề nghiệp</label>
             <input type="text" ref={jobValueInput} />
+            <p className="errormsg_none" ref={errorJob}>
+              Vui lòng nhập nghề nghiệp!
+            </p>
           </div>
           <div className="btn">
             <button type="button" onClick={handleUpdate}>
@@ -100,12 +204,18 @@ function UserUpdateInfo() {
             <button type="reset">Hủy</button>
           </div>
           {result && (
-            <p className="update-success" style={{ color: "green" }}>
+            <p
+              className="update-success"
+              style={{ color: "green", marginLeft: "20px" }}
+            >
               Update thành công!
             </p>
           )}
           {reject && (
-            <p className="update-fail" style={{ color: "red" }}>
+            <p
+              className="update-fail"
+              style={{ color: "red", marginLeft: "20px" }}
+            >
               Update thất bại!
             </p>
           )}
